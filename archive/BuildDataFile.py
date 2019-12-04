834f34cfb7cfe037dd7c18e8374e2d232458c385
Andrew Smith <andrewts129@gmail.com>
2019-12-03 22:50:20 -0500

    fixed issues with tweet parsing

--- archive/BuildDataFile.py
+++ archive/BuildDataFile.py
@@ -18,8 +18,8 @@ def parse_raw_tweet(tweet: Dict) -> Tweet:
         int(tweet['id_str']),
         tweet['text'],
         tweet['source'],
-        tweet['created_at'],
-        tweet['is_retweet']
+        tweet['created_at'],  # No point in parsing to datetime since it's just getting written to json
+        tweet['is_retweet'] or tweet['text'].startswith('RT @')  # Not all retweets are marked correctly with the bool
     )
 
 
@@ -31,12 +31,16 @@ def main():
     urls_to_query = (f'http://trumptwitterarchive.com/data/realdonaldtrump/{year}.json' for year in range(2009, 2020))
     responses = [requests.get(url).json() for url in urls_to_query]
 
-    tweets = list(sorted(flatten(responses), key=lambda tw: int(tw['id_str'])))
+    responses_flat = flatten(responses)
+    tweets = [parse_raw_tweet(response) for response in responses_flat]
+    tweets = list(sorted(tweets, key=lambda tw: tw.id))
+    tweet_dicts = [tweet._asdict() for tweet in tweets]  # For json serialization
+
     print(len(tweets))
 
     output_file = '../data/trump_tweets.ndjson'
     with open(output_file, 'w') as f:
-        ndjson.dump(tweets, f)
+        ndjson.dump(tweet_dicts, f)
 
 
 if __name__ == '__main__':
