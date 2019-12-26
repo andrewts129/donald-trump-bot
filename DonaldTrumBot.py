85d8369d83946ccde2f20e67d50b0b7e52a74702
Andrew Smith <andrewts129@gmail.com>
2019-12-25 22:51:15 -0500

    significantly improved the command line interface and made parameters configurable

--- DonaldTrumBot.py
+++ DonaldTrumBot.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
+import argparse
 import os
 import pickle
-import sys
 from timeit import default_timer as timer
+from typing import Dict
 
 import tweepy
 from dotenv import load_dotenv
@@ -16,14 +17,8 @@ from utils.TweetPoster import get_tweet_ids_to_reply_to, post_reply_tweet, shoul
 load_dotenv()
 
 
-def tweet_command() -> None:
-    # TODO configurable
-    model_file = 'data/model.pkl'
-    min_between_wakeups = 5
-    target_avg_tweets_per_day = 2.5
-    force_tweet = False
-
-    with open(model_file, 'rb') as fp:
+def tweet_command(args: Dict) -> None:
+    with open(args['model_file'], 'rb') as fp:
         model = pickle.load(fp)
 
     auth = tweepy.OAuthHandler(consumer_key=os.environ["TW_CONSUMER_KEY"],
@@ -36,47 +31,35 @@ def tweet_command() -> None:
         tweet = create_tweet(model, 200)  # TODO 240?
         post_reply_tweet(api, tweet, tweet_id)
 
-    if force_tweet or should_tweet_now(min_between_wakeups, target_avg_tweets_per_day):
+    if args['force_tweet'] or should_tweet_now(args['min_between_wakeups'], args['target_avg_tweets_per_day']):
         tweet = create_tweet(model, 240)
         post_tweet(api, tweet)
 
 
-def train_command() -> None:
-    # TODO make these configurable
-    input_file = 'data/trump_tweets.ndjson'
-    output_file = 'data/model.pkl'
-    n = 2
-
+def train_command(args: Dict) -> None:
     # TODO use partial fit method
-    model = train_model_from_file(input_file, n)
-    with open(output_file, 'wb') as fp:
+    model = train_model_from_file(args['tweet_file'], args['ngram_length'])
+    with open(args['model_file'], 'wb') as fp:
         pickle.dump(model, fp)
 
 
-def update_command() -> None:
-    output_file = 'data/trump_tweets.ndjson'  # TODO configurable
-    add_new_tweets_to_dump(output_file)
+def update_command(args: Dict) -> None:
+    add_new_tweets_to_dump(args['tweet_file'])
 
 
-def test_tweet_command() -> None:
-    # TODO make these configurable
-    input_file = 'data/trump_tweets.ndjson'
-    num_tweets = 10
-    n = 2
-    use_lazy_fit = False
-
+def test_tweet_command(args: Dict) -> None:
     train_time_start = timer()
-    model = train_model_from_file(input_file, n, use_lazy_fit)
+    model = train_model_from_file(args['tweet_file'], args['ngram_length'], args['lazy_fit'])
     train_time_total = timer() - train_time_start
 
     tweet_time_start = timer()
     tweets = []
-    for i in range(0, num_tweets):
+    for i in range(0, args['tweets_to_build']):
         tweets.append(create_tweet(model, 240))
     tweet_time_total = timer() - tweet_time_start
 
     print(f'Model training time: {train_time_total:.2f}s')
-    print(f'Tweet building time: {tweet_time_total:.2f}s')
+    print(f'Tweet building time: {tweet_time_total:.2f}s (Avg. {(tweet_time_total / len(tweets)):.2f}s / tweet)')
     print()
     print(f'Shortest Tweet: {min(len(tweet) for tweet in tweets)}')
     print(f'Longest Tweet:  {max(len(tweet) for tweet in tweets)}')
@@ -86,28 +69,33 @@ def test_tweet_command() -> None:
 
 
 def main():
-    exit_status = 1
+    parser = argparse.ArgumentParser(description='Command line interface for @DonaldTrumBot')
+    parser.add_argument('command', type=str, choices=['tweet', 'train', 'update', 'test_tweet'])
 
-    if len(sys.argv) < 2:
-        print("Specify one or more commands ('tweet', 'train', 'update', or 'test_tweet')")
-    else:
-        command = sys.argv[1]
-        if command == 'tweet':
-            tweet_command()
-            exit_status = 0
-        elif command == 'train':
-            train_command()
-            exit_status = 0
-        elif command == 'update':
-            update_command()
-            exit_status = 0
-        elif command == 'test_tweet':
-            test_tweet_command()
-            exit_status = 0
-        else:
-            print('Invalid command')
+    parser.add_argument('--model_file', type=str, default='data/model.pkl')
+    parser.add_argument('--tweet_file', type=str, default='data/trump_tweets.ndjson')
+    parser.add_argument('--min_between_wakeups', type=float, default=5)
+    parser.add_argument('--target_avg_tweets_per_day', type=float, default=2.5)
+    parser.add_argument('--force-tweet', action='store_true')
+    parser.add_argument('-n', '--ngram-length', type=int, default=2)
+    parser.add_argument('--tweets_to_build', type=int, default=10)
+    parser.add_argument('--lazy-fit', action='store_true')
 
-    exit(exit_status)
+    args = vars(parser.parse_args())
+
+    if args['command'] == 'tweet':
+        tweet_command(args)
+    elif args['command'] == 'train':
+        train_command(args)
+    elif args['command'] == 'update':
+        update_command(args)
+    elif args['command'] == 'test_tweet':
+        test_tweet_command(args)
+    else:  # This should never be reached
+        print('Invalid command')
+        exit(1)
+
+    exit()
 
 
 if __name__ == '__main__':
