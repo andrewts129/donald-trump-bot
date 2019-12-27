d7118ac7ea04ebd4c9d7c31e1fbb4ddd6ff6505e
Andrew Smith <andrewts129@gmail.com>
2019-12-26 23:43:24 -0500

    modified defaults

--- DonaldTrumBot.py
+++ DonaldTrumBot.py
@@ -74,11 +74,11 @@ def main():
 
     parser.add_argument('--model_file', type=str, default='data/model.pkl')
     parser.add_argument('--tweet_file', type=str, default='data/trump_tweets.ndjson')
-    parser.add_argument('--min_between_wakeups', type=float, default=5)
+    parser.add_argument('--min_between_wakeups', type=float, default=10)
     parser.add_argument('--target_avg_tweets_per_day', type=float, default=2.5)
     parser.add_argument('--force-tweet', action='store_true')
     parser.add_argument('--min-ngram-length', type=int, default=2)
-    parser.add_argument('--max-ngram-length', type=int, default=5)
+    parser.add_argument('--max-ngram-length', type=int, default=10)
     parser.add_argument('--tweets_to_build', type=int, default=10)
     parser.add_argument('--lazy-fit', action='store_true')
 
